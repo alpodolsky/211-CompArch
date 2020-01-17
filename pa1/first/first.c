b51b86da85fa16508e3d8644d777e70357ed9c4c
Alex Podolsky <alpodolsky1@yahoo.com>
2020-01-17 00:51:28 -0500

    second
    updated some functions, removed repetition and tried to make the code more easy to read, inserted comments

--- pa1/first/first.c
+++ pa1/first/first.c
@@ -1,10 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-
+//takes in an array, even number count, and arraysize(technically the last index not size)
 void arraySort(int array[], int e, int arrSize){
     int i, j ,temp, arrS= arrSize-1;
-
     //sort odd
     for(i=0;i<arrSize-e;i++){
         for(j=0;j<arrS-e-i;j++){
@@ -15,7 +14,6 @@ void arraySort(int array[], int e, int arrSize){
             }
         }
     }
-
     //Sort even
     for(i=arrSize-e;i<arrS;i++){
         for(j=arrSize-e;j<arrS;j++){
@@ -31,29 +29,32 @@ void arraySort(int array[], int e, int arrSize){
 //main reads the files correctly
 int main(int argc, char** argv) {
 
-    int arrSize=0, i, j ,eCount;
-
+    int arrSize=0, i=0, j=0 ,eCount=0;
+    //points to a file and opens it with the 'read' permission
     FILE* fpoint = fopen(argv[1], "r");
     
-
+    //looks for a number and makes that the arraysize
     fscanf(fpoint, "%d", &arrSize);
     int *array;
+    //allocate space for the array
     array = (int*)malloc(arrSize*sizeof(int));
 
-
-    
+    //scans for a number up to the size of the array
+        //takes not if the number is even on each iteration
+        //can be odd instead, its purpose stays the same
     for(i = 0; i <arrSize; i++){
         fscanf(fpoint, "%d", &array[i]);
-        
         if(abs(array[i]%2)==0){
             eCount++;
         }
     }
+    //close the file because we dont need it anymore
+    fclose(fpoint);
 
     int temp, arrS=arrSize-1;
-    
     for(i=0;i<arrS;i++){
         if(abs(array[i]%2)==0){
+            //look for the first odd entry in the array and swap them
             for(j=i+1;j<arrS&&abs(array[j]%2)!=1;j++);
             temp = array[i];
             array[i]=array[j];
@@ -61,13 +62,12 @@ int main(int argc, char** argv) {
         }
     }
     
-    fclose(fpoint);
-    
+    //send relevant information to sort function
     arraySort(array, eCount, arrSize);
-    
+    //print the results
     for(i = 0; i <arrSize;i++){
         printf("%d\t", array[i]);
     }
-    free(array);
+    free(array);//free the allocated memory
     return 0;
 }

bc15e8bebcb950d247c0ca51b6a0dbdee8d8ca44
Alex Podolsky <alpodolsky1@yahoo.com>
2020-01-17 00:58:24 -0500

    fixed
    cleaned up the code, removed repitition, added helpful comments

--- pa1/second/second.c
+++ pa1/second/second.c
@@ -7,83 +7,88 @@ struct Node{
     //pointer for next in a node
     struct Node* next;
 };
+void insert(struct Node **hash, int key, int number){
+    //hash[key] is empty
+    if(hash[key]==NULL){
+        hash[key] = (struct Node*)malloc(sizeof(struct Node));
+        hash[key]->val=number;
+        hash[key]->next=NULL;
+        printf("inserted\n");
+        return;        
+    }
+    //hash[key] is not empty
+    else {
+        struct Node*temp, *insert;
+        //loops thru list until NULL node, checking if current node's value is == number
+        for(temp = hash[key]; temp!=NULL;temp =temp->next){
+            //if number is present already
+            if(temp->val==number){
+                printf("duplicate\n");
+                return;
+            }         
+        }
+        //just incase it is anything else
+        if(temp==NULL){
+            insert = (struct Node*)malloc(sizeof(struct Node));
+            insert->val = number;
+            temp->next=insert;
+            insert->next = NULL;          
+            printf("inserted\n");
+            return;
+        } 
+        else{
+            printf("This should never occur\n");
+            return;
+        }      
+    }
+};
+void search(struct Node**hash, int key, int number){
+    //if number is not present
+    if(hash[key]==NULL){
+        printf("absent\n");
+        return;
+    }
+    //if key exists
+    else if(hash[key]!=NULL){
+        struct Node*temp;
+        for(temp = hash[key]; temp!=NULL;temp=temp->next){
+            if(temp->val==number){ 
+                printf("present\n");
+                return;
+            }                    
+        }
+        if(temp==NULL){
+            printf("absent\n");
+            return;
+        }  
+    }
+};
 int main(int argc, char** argv){
+    //init hash type struct(really array of pointer nodes that will each point to their own LL's)
     struct Node **hash= (struct Node**)malloc(10000*sizeof(struct Node));
     struct Node *start = NULL;
 
     FILE* fpoint = fopen(argv[1], "r");
 
+    char letter;//used later to indicate command(either insert,'i' or search,'s')
     char letter;
     int number, i;
     //sets all hash values to NULL
     for(i=0;i<10000;i++){
         hash[i]=NULL;
     }
-    
+    //loops until end of file
+
     while(fscanf(fpoint, "%c %d", &letter,&number)!=EOF){
+        //hash key is the modded value of number by the hash table size which is 10k in this case
         int key = abs(number%10000);
-        
+
         if(letter =='i'){
-            //hash[key] is empty
-
-            if(hash[key]==NULL){
-                start = (struct Node*)malloc(sizeof(struct Node));
-                start->val=number;
-                start->next=NULL;
-                hash[key]=start;
-                printf("inserted\n");
-                
-            }
-            //hash[key] is not empty
-            else {
-                struct Node*temp, *insert;
-                if(hash[key]->next==NULL){
-                    if(hash[key]->val==number){
-                        printf("duplicate\n");
-                    }
-                }
-                //loops thru list
-                for(temp = hash[key]; temp->next!=NULL;temp =temp->next){
-                    //if number is present already
-                    if(temp->val==number){
-                        printf("duplicate\n");
-                        break;
-                    }
-                    
-                }
-                if(temp->val!=number){
-                    insert = (struct Node*)malloc(sizeof(struct Node));
-                    insert->val = number;
-                    //sets the value that stopped the loop to insert
-                    temp->next=insert;
-                    insert->next = NULL;
-                       
-                    printf("inserted\n");
-                }
-                
-            }
-        }
+            insert(hash, key, number);
+        } 
+        
         else if(letter=='s'){
-            //if number is not present
-            if(hash[key]==NULL){
-                printf("absent\n");
-            }
-
-            //if key exists
-            else if(hash[key]!=NULL){
-                struct Node*temp;
-
-                
-                for(temp = hash[key]; temp!=NULL;temp=temp->next){
-                    if(temp->val==number){ 
-                        printf("present\n");
-                        break;
-                    }                    
-                }
-                if(temp==NULL){
-                    printf("absent\n");
-                }  
-            }
+            search(hash, key, number); 
         }
     }
     fclose(fpoint);
